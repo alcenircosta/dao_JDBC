5ece01ba09789096ee7516dedbee3ecc8f310398
Alcenir <alcenir.1994@gmail.com>
2021-10-01 21:05:07 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-Department obj = new Department(1,"Boooks");
-System.out.println(obj);
+		Locale.setDefault(Locale.US);
+		Department obj = new Department(1, "Boooks");
+		Seller seller = new Seller(1,"João","joao@gmail.com",new Date(),1250.00,obj);
+		
+		System.out.println(obj);
+		System.out.println(seller);
+		
+		
 	}
 }
