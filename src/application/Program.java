28166b919e9d7e42133482fdb53abe810e5afb09
Alcenir <alcenir.1994@gmail.com>
2021-10-02 00:06:13 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,15 +10,9 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Department obj = new Department(1, "Boooks");
-		Seller seller = new Seller(1,"João","joao@gmail.com",new Date(),1250.00,obj);
 		SellerDao sellerDao = DAOFactory.createSellerDao();
-		
-		
-		System.out.println(obj);
+		Seller seller = sellerDao.findByID(3);
 		System.out.println(seller);
-		
-		
+
 	}
 }
